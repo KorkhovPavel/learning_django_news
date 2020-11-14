5f685014476cda7c07bc5b79f8b79312b2d8964b
korkhovpavel <korkhov.pi@gmail.com>
2020-11-14 20:46:22 +0300

    Controllers and routes

---
+++ mysite/news/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    print(request)
+    return HttpResponse('Hello word')
+
+def test(request):
+    print(request)
+    return HttpResponse('<h1>Hello word</h1>')
