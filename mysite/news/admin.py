14a516b94a6e07ca96a10de62f5c956bf63a6e03
KorkhovPavel <korkhov.pi@gmail.com>
2020-11-16 12:59:31 +0300

    Template appearance

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -5,7 +5,7 @@ from .models import News,Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','title','content','created_at','updated_at','is_published','photo','category')
+    list_display = ('id','title','created_at','updated_at','is_published','photo','category')
     list_display_links = ('id','title')
     search_fields = ('content','title',)
     list_filter = ('is_published','category',)
