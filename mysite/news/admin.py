9bd04fbdaa4be799a69034fdf3c2ce46c43921f9
KorkhovPavel <korkhov.pi@gmail.com>
2020-11-16 11:43:48 +0300

    Model relationships

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News,Category
 
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','title','content','created_at','updated_at','is_published','photo')
+    list_display = ('id','title','content','created_at','updated_at','is_published','photo','category')
     list_display_links = ('id','title')
-    search_fields = ('content','title')
+    search_fields = ('content','title',)
+    list_filter = ('is_published','category',)
+    list_editable = ('is_published',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+    search_fields = ('title',)
 
 admin.site.register(News,NewsAdmin)
+admin.site.register(Category,CategoryAdmin)
