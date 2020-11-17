8fa0d2c4fbfa875861472a97eb4047f724d83fe0
KorkhovPavel <korkhov.pi@gmail.com>
2020-11-17 23:27:52 +0300

    Reverse address resolution 21

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -8,19 +9,25 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category',on_delete=models.PROTECT, null=True,verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
 
+
 class Category(models.Model):
-    title = models.CharField(max_length=150 , db_index=True)
+    title = models.CharField(max_length=150, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News, Category
 
 
@@ -16,9 +14,16 @@ def index(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
+    category = get_object_or_404 (Category, pk=category_id)
+    # category = Category.objects.get(pk=category_id)
     context = {
-        'news' : news,
-        'category' : category
+        'news': news,
+        'category': category
     }
     return render(request, 'news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404 (News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
