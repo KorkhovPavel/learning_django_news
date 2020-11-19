bfe025fb40c52590d12d0cb967967636e72b4138
KorkhovPavel <korkhov.pi@gmail.com>
2020-11-19 15:49:48 +0300

    class CreateView 30

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
 from .forms import NewsForm
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 class HomeNews(ListView):
     model = News
@@ -27,11 +27,11 @@ class NewsByCategory(ListView):
     allow_empty = False
 
     def get_queryset(self):
-        return News.objects.filter(category_id = self.kwargs['category_id'],is_published=True)
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = News.objects.get(pk = self.kwargs['category_id'],is_published=True)
+        context['title'] = News.objects.get(pk=self.kwargs['category_id'], is_published=True)
         return context
 
 
@@ -42,6 +42,10 @@ class ViewNews(DetailView):
     context_object_name = 'news_item'
 
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
 
 # def index(request):
 #     print(request)
@@ -64,20 +68,20 @@ class ViewNews(DetailView):
 #     return render(request, 'news/category.html', context=context)
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-            # print(form.cleaned_data)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#             # print(form.cleaned_data)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
